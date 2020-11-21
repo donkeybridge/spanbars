f8f04d33d274e8654e006b8eee92cea352778c8d
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2020-11-21 19:30:52 +0000

    lib/spanbars.rb: applied linting to main includer

--- lib/spanbars.rb
+++ lib/spanbars.rb
@@ -1,17 +1,6 @@
-#!/usr/bin/env ruby
-##
+# frozen_string_literal: true
 
 require 'slop'
 require 'csv'
 require 'colorize'
-require 'ezconfig' 
-
-THIS_MAIN_FILE = File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
-require File.dirname(THIS_MAIN_FILE) + '/spancluster.rb'
-require File.dirname(THIS_MAIN_FILE) + '/spanbarprocessor.rb'
-require File.dirname(THIS_MAIN_FILE) + '/spanbar.rb'
-#require File.dirname(THIS_MAIN_FILE) + '/trendstate.rb'
-
-
-
-
+%i[spancluster spanbarprocessor spanbar trendstate].each { |f| require "#{__dir__}/#{f}" }
